386d3f424d9adab2a7069fd1da25d5f6435c6d03
drew york <119648808+dre-yo@users.noreply.github.com>
2024-09-28 11:15:03 -0700

    added comments to solution

--- solution/search_rotated_array.cc
+++ solution/search_rotated_array.cc
@@ -19,17 +19,24 @@ int Search(std::vector<int>& nums, int target) {
   int result = -1;
   while (l <= r) {
     m = (l + r) / 2;
+    // if target is found
     if (nums[m] == target) {
       result = m;
       break;
     }
+
+    // if left side is sorted
     if (nums[l] <= nums[m]) {
+      // if target is in the left side
       if (target >= nums[l] && target < nums[m]) {
         r = m - 1;
       } else {
         l = m + 1;
       }
+
+      // if right side is sorted
     } else {
+      // if target is in the right side
       if (target <= nums[r] && target > nums[m]) {
         l = m + 1;
       } else {
